48927191c42bf0f988415fdde5d59fda68acfde4
kavyashree h patwardhan <storeimage@gmail.com>
2022-11-30 22:08:39 +0530

    files added

---
+++ script.js
@@ -0,0 +1,11 @@
+const name = document.getElementById('name')
+const lname = document.getElementById('lname')
+const email = document.getElementById('email')
+const reason = document.getElementById('reason')
+const form = document.getElementById('form')
+const errorElement = document.getElementById('error')
+
+if(name==" ")
+{
+    alert=console.log("name reuires")
+}
